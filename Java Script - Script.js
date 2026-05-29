6617ef7eb2ffc55568a12b68749b0a5c0a014c76
DayaneSilva1025 <139177183+DayaneSilva1025@users.noreply.github.com>
2026-05-29 09:09:23 -0300

    Create Java Script - Script.js

---
+++ Java Script - Script.js
@@ -0,0 +1,19 @@
+// BOTÃO COMPRAR
+
+$(".comprar").click(function () {
+  alert("Produto adicionado ao carrinho!");
+});
+
+// FORMULÁRIO DE NOVIDADES
+
+$("#btnNovidade").click(function () {
+  alert("Cadastro realizado com sucesso!");
+});
+
+// LOGIN
+
+$("#formLogin").submit(function (event) {
+  event.preventDefault();
+
+  alert("Login realizado com sucesso!");
+});
